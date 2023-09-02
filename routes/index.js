23bebb3e74b74aeb0e7fb433b1dad9c183ba8015
Adio Adebayo <zillionaire234@gmail.com>
2023-09-02 13:49:13 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
-import express from 'express'; 
-import AppController from '../controllers/AppController'; 
+// routes/index.js
+import express from 'express';
+import AppController from '../controllers/AppController';
 
-const router = express.Router(); 
+const router = express.Router();
 
 // Define routes
-router.get('/status', AppController.getStatus); // Make sure AppController.getStatus is a valid callback function.
-router.get('/stats', AppController.getStats);   // Make sure AppController.getStats is a valid callback function.
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
 
 export default router;
