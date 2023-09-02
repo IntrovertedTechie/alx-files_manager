aadce8316f8904f4f34566bf75fd8a1d90b7cfcf
Adio Adebayo <zillionaire234@gmail.com>
2023-09-02 17:29:17 +0100

    Update AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,27 +1,20 @@
-import { checkRedis } from '../utils/redis'; // Import your Redis utility  
-import { checkDB } from '../utils/db'; // Import your DB utility  
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
 
-const AppController = {  
-  getStatus: async (req, res) => {  
-    try {  
-      // Your logic for the /status route goes here.
-      res.status(200).json({ "status": "ok" });  
-    } catch (error) {  
-      res.status(500).json({ "status": "error" });  
-    }  
-  },
-  
-  getStats: async (req, res) => {  
-    try {  
-      // Simulate counting users and files (you need to replace this with actual database queries) 
-      const userCount = 4; // Replace with your user count logic 
-      const fileCount = 30; // Replace with your file count logic  
-  
-      res.status(200).json({ "users": userCount, "files": fileCount });  
-    } catch (error) {  
-      res.status(500).json({ "users": 0, "files": 0 });  
-    }  
-  }  
-}; 
+class AppController {
+  static getStatus(request, response) {
+    return response.status(200).send(
+      `{"redis": ${redisClient.isAlive()}, "db": ${dbClient.isAlive()}}`,
+    );
+  }
 
-export default AppController;
+  static async getStats(request, response) {
+    const nbusers = await dbClient.nbUsers();
+    const nbfiles = await dbClient.nbFiles();
+    return response.status(200).send(
+      `{"users": ${nbusers}, "files": ${nbfiles}}`,
+    );
+  }
+}
+
+module.exports = AppController;
