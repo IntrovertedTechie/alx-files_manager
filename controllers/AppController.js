13e9acb2b3fdc1140f2f08ec07e1842bd33682b4
Adio Adebayo <zillionaire234@gmail.com>
2023-09-02 11:18:26 +0100

    Update AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,32 +1,32 @@
-import redisClient from '../utils/redis';
-import dbClient from '../utils/db';
+const { checkRedis, checkDB } = require('../utils'); // Import your utility functions
 
-class AppController {
-  /**
-   * should return if Redis is alive and if the DB is alive too
-   * by using the 2 utils created previously:
-   * { "redis": true, "db": true } with a status code 200
-   */
-  static getStatus(request, response) {
-    const status = {
-      redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
-    };
-    response.status(200).send(status);
+const AppController = {
+  getStatus: async (req, res) => {
+    try {
+      const redisStatus = await checkRedis();
+      const dbStatus = await checkDB();
+      
+      if (redisStatus && dbStatus) {
+        res.status(200).json({ "redis": true, "db": true });
+      } else {
+        res.status(500).json({ "redis": false, "db": false });
+      }
+    } catch (error) {
+      res.status(500).json({ "redis": false, "db": false });
+    }
+  },
+
+  getStats: async (req, res) => {
+    try {
+      // Use your collections (e.g., users and files) to count users and files
+      const userCount = await UserCollection.countDocuments();
+      const fileCount = await FileCollection.countDocuments();
+
+      res.status(200).json({ "users": userCount, "files": fileCount });
+    } catch (error) {
+      res.status(500).json({ "users": 0, "files": 0 });
+    }
   }
+};
 
-  /**
-   * should return the number of users and files in DB:
-   * { "users": 12, "files": 1231 }
-   *  with a status code 200
-   */
-  static async getStats(request, response) {
-    const stats = {
-      users: await dbClient.nbUsers(),
-      files: await dbClient.nbFiles(),
-    };
-    response.status(200).send(stats);
-  }
-}
-
-export default AppController;
+module.exports = AppController;
