13a1c754a3d50c0c4c75e2663d829e421e7532d3
Adio Adebayo <zillionaire234@gmail.com>
2023-09-02 14:34:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Load routes from routes/index.js
+// Import your routes module
 const routes = require('./routes');
+
+// Use the routes middleware
 app.use('/', routes);
 
 app.listen(PORT, () => {
