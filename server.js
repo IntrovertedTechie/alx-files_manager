7f017f1fb6975c5bfbd54156e0c67fa55424f766
Adio Adebayo <zillionaire234@gmail.com>
2023-09-02 11:19:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 5000;
 const routes = require('./routes');
 app.use('/', routes);
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
