58d86790fc9dd1f6ce47c0412411b321685694a0
Adio Adebayo <zillionaire234@gmail.com>
2023-09-02 14:07:21 +0100

    Update AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,20 +1,27 @@
-import { checkRedis } from '../utils/redis'; // Import your Redis utility 
-import { checkDB } from '../utils/db'; // Import your DB utility 
+import { checkRedis } from '../utils/redis'; // Import your Redis utility  
+import { checkDB } from '../utils/db'; // Import your DB utility  
 
-const AppController = { 
-  // ... (your getStatus function remains the same)
-
-  getStats: async (req, res) => { 
-    try { 
-      // Simulate counting users and files (you need to replace this with actual database queries)
-      const userCount = 4; // Replace with your user count logic
-      const fileCount = 30; // Replace with your file count logic 
-
-      res.status(200).json({ "users": userCount, "files": fileCount }); 
-    } catch (error) { 
-      res.status(500).json({ "users": 0, "files": 0 }); 
-    } 
-  } 
+const AppController = {  
+  getStatus: async (req, res) => {  
+    try {  
+      // Your logic for the /status route goes here.
+      res.status(200).json({ "status": "ok" });  
+    } catch (error) {  
+      res.status(500).json({ "status": "error" });  
+    }  
+  },
+  
+  getStats: async (req, res) => {  
+    try {  
+      // Simulate counting users and files (you need to replace this with actual database queries) 
+      const userCount = 4; // Replace with your user count logic 
+      const fileCount = 30; // Replace with your file count logic  
+  
+      res.status(200).json({ "users": userCount, "files": fileCount });  
+    } catch (error) {  
+      res.status(500).json({ "users": 0, "files": 0 });  
+    }  
+  }  
 }; 
 
 export default AppController;
