e47e864d16bf0408d4d8f5cd3e3196cfc31ee1ff
Adio Adebayo <zillionaire234@gmail.com>
2023-09-03 07:57:13 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,9 @@
-const express = require('express');
-const AppController = require('../controllers/AppController');
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
 
-const app = express();
+const router = Router();
 
-// Define a common middleware function that will be executed for all routes
-app.all('*', (req, res, next) => {
-  // Perform common operations or checks here
-  // For example, you can add authentication logic here
-  
-  // Continue to the next middleware or route handler
-  next();
-});
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
 
-// Define your routes
-app.get('/status', (req, res) => {
-  AppController.getStatus(req, res);
-});
-
-app.get('/stats', (req, res) => {
-  AppController.getStats(req, res);
-});
-
-// Start the Express server
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Express Server app running on http://localhost:${port}/`);
-});
+export default router
