5bbea235f6237396a6bab08161be6945335eb73f
Adio Adebayo <zillionaire234@gmail.com>
2023-09-03 10:12:20 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController'; // Import the UsersController
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
-export default router
+// Add a new endpoint for creating a user
+router.post('/users', UsersController.postNew);
+
+export default router;
