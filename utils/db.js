2f81428e36d90511037b69e4fec6c7cbc925bd68
Adio Adebayo <zillionaire234@gmail.com>
2023-09-04 14:42:49 +0100

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,32 +1,39 @@
 import { MongoClient } from 'mongodb';
 
+// Set up URL and database name
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || '27017';
+const url = `mongodb://${host}:${port}`;
+const dbName = process.env.DB_DATABASE || 'files_manager';
+
 class DBClient {
   constructor() {
-    const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'files_manager';
-
-    this.client = new MongoClient(`mongodb://${host}:${port}`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
 
     this.client.connect(async (err) => {
       if (err) {
         console.error(`MongoDB connection error: ${err}`);
       } else {
-        console.log('MongoDB connected');
+        console.log('Connected to mongodb server');
         // Create the 'files_manager' database if it doesn't exist
-        const db = this.client.db(database);
-        await db.createCollection('users'); // Create a collection (table) for users
-        await db.createCollection('files'); // Create a collection (table) for files
+        await this.createDatabase(dbName);
+        this.db = this.client.db(dbName);
       }
     });
+  }
+
+  // Helper function to create the database if it doesn't exist
+  async createDatabase(databaseName) {
+    const adminDb = this.client.db('admin');
+    const databasesList = await adminDb.admin().listDatabases();
+
+    if (!databasesList.databases.find(db => db.name === databaseName)) {
+      await adminDb.admin().createDatabase(databaseName);
+    }
   }
 
   // ... (rest of the methods are similar)
 }
 
 const dbClient = new DBClient();
-
 export default dbClient;
