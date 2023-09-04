a924bb9d0a492709c342de1eecfac452d649a903
Adio Adebayo <zillionaire234@gmail.com>
2023-09-04 17:18:39 +0100

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,3 @@
-// module contains class DBClient:-nbUsers, nbFiles, isAlive methods
 const { MongoClient } = require('mongodb');
 
 class DBClient {
@@ -8,9 +7,22 @@ class DBClient {
     const port = process.env.DB_PORT || '27017';
     const database = process.env.DB_DATABASE || 'files_manager';
     const url = `mongodb://${host}:${port}/`;
+
     MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
-      if (error) throw (error);
+      if (error) throw error;
       this.dbName = client.db(database);
+      
+      // Check if the database exists, and create it if missing
+      this.dbName.listCollections().toArray((err, collections) => {
+        if (err) throw err;
+        const dbExists = collections.some(collection => collection.name === 'users' || collection.name === 'files');
+        
+        if (!dbExists) {
+          // Create the necessary collections (users and files)
+          this.dbName.createCollection('users');
+          this.dbName.createCollection('files');
+        }
+      });
     });
   }
 
@@ -28,5 +40,6 @@ class DBClient {
     return nbfiles;
   }
 }
+
 const dbClient = new DBClient();
 module.exports = dbClient;
