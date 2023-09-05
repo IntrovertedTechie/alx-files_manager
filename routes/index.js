fc1f7ae653c9b0634e66249b400692a040e5cc34
Adio Adebayo <zillionaire234@gmail.com>
2023-09-05 13:30:02 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 import{ Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController'; // Import the AuthController
 
 const router = Router();
 
@@ -10,8 +9,5 @@ router.get('/stats', AppController.getStats);
 
 // Add new authentication and user-related endpoints
 router.post('/users', UsersController.postNew);
-router.get('/users/me', UsersController.getMe);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
 
 export default router;
